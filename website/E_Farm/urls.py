bb80639a614f989d2085dcf842d34c670e4ac941
RiddheshVeling <68471554+RiddheshVeling@users.noreply.github.com>
2023-03-26 15:42:01 +0530

    Add files via upload

---
+++ website/E_Farm/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', home),
+    path("farmer-registration/",farmerReg),
+    path("farmer-login/",farmerLog),
+    path("add-product/",addPro),
+    path("user-registration/",userReg),
+    path("user-login/",userLog),
+    path("about-us/",aboutUs),
+    path("contact-us/",contactUs),
+    path("cart/",cart),
+    path("checkout/",checkout),
+
+
+]
